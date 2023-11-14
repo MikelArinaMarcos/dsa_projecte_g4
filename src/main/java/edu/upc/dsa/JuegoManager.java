c457121116fb8bfc9de8d7b7f0eb1639dcb72014
Pedro <pedrojordan2001@gmail.com>
2023-11-14 12:23:29 +0100

    pom.xml definitivo

--- src/main/java/edu/upc/dsa/JuegoManager.java
+++ src/main/java/edu/upc/dsa/JuegoManager.java
@@ -1,4 +1,10 @@
 package edu.upc.dsa;
 
+import edu.upc.dsa.models.Usuario;
+import edu.upc.dsa.models.VOCredenciales;
+
+import java.util.List;
 public interface JuegoManager {
+    public Usuario RegistrarUsuario(String Username, String Mail, String Name, String Lastname, String Password);
+    public Usuario LogIn(VOCredenciales credencialesu);
 }
--- src/main/java/edu/upc/dsa/services/UsuarioService.java
+++ src/main/java/edu/upc/dsa/services/UsuarioService.java
@@ -1,5 +1,20 @@
 package edu.upc.dsa.services;
 
+import edu.upc.dsa.JuegoManager;
+import edu.upc.dsa.JuegoManagerImpl;
+import edu.upc.dsa.models.Usuario;
+import edu.upc.dsa.models.VOCredenciales;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 public class UsuarioService {
 
 }
