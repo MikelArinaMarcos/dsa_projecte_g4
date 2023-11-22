6e661f63966e2efac0fd4a8baec8ba0bc6e14355
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-11-22 16:34:26 +0100

    Cambios en el register

---
+++ public/scripts/register.js
@@ -0,0 +1,34 @@
+/******************************************/
+/* JAVASCRIPT PARA EL PROCESO DE REGISTRO */
+/******************************************/
+
+$(document).ready(function(){
+    $('#signupButton').click(function(event){
+        event.preventDefault();
+
+        // Nos creamos variables para recoger los datos del formulario.
+
+        var name = $('#name').val();
+        var password = $('#password').val();
+        var mail = $('#mail').val();
+        var username = $('#username').val();
+        var lastName = $('#lastName').val();
+        var body = {"name": name, "password": password, "mail": mail, "username": username, "lastName": lastName};
+
+        $.post({
+            url: '/dsaApp/users/register',
+            data: JSON.stringify(body),
+            contentType: 'application/json; charset=utf8'
+        })
+            .done(function (data, status){
+                alert("***************** Usuario registrado correctamente.");
+                location.href = "/shop.html";
+            })
+            .fail(function(xhr, err){
+                console.log("ERRO", err);
+                alert("Por favor, revisa los datos introducidos");
+                //location.reload();
+            });
+        return true;
+    });
+});
