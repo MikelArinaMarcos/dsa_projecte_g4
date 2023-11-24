cefbd12418af926c43436386a56d8f9f1cba37e4
Dell <aran.huarte@estudiantat.upc.edu>
2023-11-24 11:47:05 +0100

    todo muy mal

--- src/main/java/edu/upc/dsa/JuegoManagerImpl.java
+++ src/main/java/edu/upc/dsa/JuegoManagerImpl.java
@@ -6,10 +6,6 @@ import edu.upc.dsa.models.VOCredenciales;
 import org.apache.log4j.Logger;
 
 import java.util.HashMap;
-<<<<<<< HEAD
-=======
-import java.util.LinkedHashMap;
->>>>>>> 54dbfb67f107444cc37fc18782cdc9a37fbc6e69
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,12 +13,7 @@ import java.util.stream.Collectors;
 public class JuegoManagerImpl implements JuegoManager{
     private static JuegoManager instance;
     protected List<Usuario> Usuarios;
-<<<<<<< HEAD
     protected HashMap<String, Usuario> lUsuarios = new HashMap<String, Usuario>();
-=======
-    protected HashMap<String, Usuario> LUsuarios = new HashMap<String, Usuario>();
-    private JuegoManagerImpl(){this.LUsuarios = new LinkedHashMap<>();}
->>>>>>> 54dbfb67f107444cc37fc18782cdc9a37fbc6e69
     protected List<Personaje> Personajes;
     final static Logger logger = Logger.getLogger(JuegoManagerImpl.class);
     public  static JuegoManager getInstance(){
@@ -67,49 +58,23 @@ public class JuegoManagerImpl implements JuegoManager{
         }
         logger.info("new user "+u.getName());
         this.lUsuarios.put(u.getMail(),u);
-        logger.info("new user"+u);
-        this.LUsuarios.put(u.getMail(),u);
-        logger.info("new user added");
-        return u;
-    }
-    public int RegistrarUsuario(Usuario usuarios){
-        Usuario U=new Usuario(usuarios.getUsername(),usuarios.getMail(),usuarios.getName(),usuarios.getLastname(),usuarios.getPassword());
-        for (HashMap.Entry<String, Usuario> entry : LUsuarios.entrySet()) {
-            if (U.getMail().equals(LUsuarios.get(entry.getKey()).getMail())){
-                return 1;
-            }
-            if (U.getUsername().equals(LUsuarios.get(entry.getKey()).getUsername())){
-                return 2;
-            }
-        }
-        logger.info("new user"+U);
-        this.LUsuarios.put(U.getMail(),U);
->>>>>>> 54dbfb67f107444cc37fc18782cdc9a37fbc6e69
         logger.info("new user added");
         return 0;
     }
     @Override
-<<<<<<< HEAD
     public Usuario LogIn(VOCredenciales credencialesu) {
         logger.info("login(" + credencialesu +")");
         if (lUsuarios.containsKey(credencialesu.getMail())){
             Usuario U= lUsuarios.get(credencialesu.getMail());
             logger.info("LogIn("+credencialesu.getMail()+")"+U.getUsername());
-=======
-    public Usuario LogIn(VOCredenciales credenciales) {
-        logger.info("login(" + credenciales +")");
-        if (LUsuarios.containsKey(credenciales)){
-            Usuario U= LUsuarios.get(credenciales.getMail());
-            logger.info("LogIn("+credenciales+")"+U);
->>>>>>> 54dbfb67f107444cc37fc18782cdc9a37fbc6e69
             return U;
         }
-        logger.warn(credenciales+"not found");
+        logger.warn(credencialesu+"not found");
         return null;
     }
     public VOCredenciales getCredenciales(Usuario U){
         logger.info("getCredenciales("+U+")");
-        VOCredenciales VOC=new VOCredenciales(U.getMail(),U.getPassword());
+        VOCredenciales VOC=new VOCredenciales(U.getUsername(),U.getPassword());
         return VOC;
     }
     public Usuario getUser(VOCredenciales credenciales){
@@ -124,28 +89,16 @@ public class JuegoManagerImpl implements JuegoManager{
         logger.warn(credenciales+"not found");
         return null;
     }
-<<<<<<< HEAD
     public List<Usuario> getallusers() {
         return this.lUsuarios.values().stream().collect(Collectors.toList());
-=======
-    public HashMap<String, Usuario> getallusers() {
-        return this.LUsuarios;
->>>>>>> 54dbfb67f107444cc37fc18782cdc9a37fbc6e69
     }
     @Override
     public Usuario deleteUsuario(VOCredenciales credenciales) {
         logger.info("deleteUsuario(" + credenciales +")");
-<<<<<<< HEAD
-        if (lUsuarios.containsKey(credenciales)){
-            Usuario U= lUsuarios.get(credenciales);
-            lUsuarios.remove(credenciales);
+        if (lUsuarios.containsKey(credenciales.getMail())){
+            Usuario U= lUsuarios.get(credenciales.getMail());
+            lUsuarios.remove(credenciales.getMail());
             logger.info("deleteUsuario("+credenciales+")"+U);
-=======
-        if (LUsuarios.containsKey(credenciales.getMail())){
-            Usuario U= LUsuarios.get(credenciales.getMail());
-            LUsuarios.remove(credenciales.getMail());
-            logger.info("deleteUsuario("+credenciales.getMail()+")"+U);
->>>>>>> 54dbfb67f107444cc37fc18782cdc9a37fbc6e69
             return U;
         }
         logger.warn(credenciales+"not found");
