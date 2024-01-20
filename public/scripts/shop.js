3afcc6dcd84744422ae2a57ecf133772823d6ed4
Pedro <pedrojordan2001@gmail.com>
2024-01-20 15:38:32 +0100

    P63.Web

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -1,42 +1,3 @@
-$(document).ready(function(){
-    $('#listaObjetos').on('click', '.comprarBtn', function (event) {
-        event.preventDefault();
-        var objeto = $(this).closest('.item').data('objeto');
-        // Obtener los datos de los objetos
-        var id = objeto.id;
-        var rareza = objeto.rareza;
-        var nombre = objeto.nombre;
-        var precio = objeto.precio;
-        var damage = objeto.damage;
-        var url = objeto.url;
-        const mail = localStorage.getItem('mail'); // Reemplaza esto con el correo electrónico que deseas enviar al servidor
-        var body = {
-            "id": id,
-            "rareza": rareza,
-            "nombre": nombre,
-            "precio": precio,
-            "damage": damage,
-            "url": url
-        };
-
-        // Enviar la solicitud POST al servidor para el inicio de sesión
-        $.post({
-            url:  `/dsaApp/tienda/comprarObjeto/${encodeURIComponent(mail)}`, // Ajusta la URL según tu estructura de carpetas y rutas
-            data: JSON.stringify(body),
-            contentType: 'application/json; charset=utf8'
-        })
-            .done(function (data, status){
-                alert("¡Añadido a tu inventario!");
-                location.href = "/principal.html";
-            })
-            .fail(function(xhr, err){
-                console.log("ERROR", err);
-                alert("No se ha podido comprar");
-            });
-        return true;
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     // Esperamos a que el contenido del DOM esté cargado antes de ejecutar el código
 
@@ -48,49 +9,82 @@ document.addEventListener('DOMContentLoaded', function () {
             mostrarListaObjetos(data);
         })
         .catch(error => console.error('Error al obtener la lista de objetos:', error));
+
+    $('#listaObjetos').on('click', '.comprarBtn', function (event) {
+        event.preventDefault();
+        var objeto = $(this).closest('.item').data('objeto');
+        comprarObjeto(objeto);
+        return true;
+    });
 });
-
-function crearElementoObjeto(objeto) {
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('item');
-
-    const nombreObjetoHeading = document.createElement('h2');
-    nombreObjetoHeading.classList.add('nombreObjeto');
-    nombreObjetoHeading.textContent = objeto.nombre;
-
-    const descripcionP = document.createElement('p');
-    descripcionP.classList.add('descripcion');
-    descripcionP.textContent = objeto.id;
-
-    const atributoPrecioP = document.createElement('p');
-    atributoPrecioP.classList.add('atributo');
-    atributoPrecioP.textContent = `Precio: $${objeto.precio}`;
-
-    const atributoDanioP = document.createElement('p');
-    atributoDanioP.classList.add('atributo');
-    atributoDanioP.textContent = `Damage: ${objeto.damage}`;
-
-    const comprarBtn = document.createElement('button');
-    comprarBtn.classList.add('comprarBtn');
-    comprarBtn.textContent = 'Comprar';
-
-    // Agregamos los elementos al contenedor
-    itemDiv.appendChild(nombreObjetoHeading);
-    itemDiv.appendChild(descripcionP);
-    itemDiv.appendChild(atributoPrecioP);
-    itemDiv.appendChild(atributoDanioP);
-    itemDiv.appendChild(comprarBtn);
-
-    return itemDiv;
-}
-
 function mostrarListaObjetos(listaObjetos) {
     const listaObjetosContainer = document.getElementById('listaObjetos');
 
     // Iteramos sobre la lista de objetos y creamos dinámicamente las secciones en el DOM
     listaObjetos.forEach(objeto => {
-        const itemDiv = crearElementoObjeto(objeto);
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('item');
+
+        const nombreObjetoHeading = document.createElement('h2');
+        nombreObjetoHeading.classList.add('nombreObjeto');
+        nombreObjetoHeading.textContent = objeto.nombre;
+
+        const descripcionP = document.createElement('p');
+        descripcionP.classList.add('descripcion');
+        descripcionP.textContent = objeto.id;
+
+        const atributoPrecioP = document.createElement('p');
+        atributoPrecioP.classList.add('atributo');
+        atributoPrecioP.textContent = `Precio: $${objeto.precio}`;
+
+        const atributoDanioP = document.createElement('p');
+        atributoDanioP.classList.add('atributo');
+        atributoDanioP.textContent = `Damage: ${objeto.damage}`;
+
+        const comprarBtn = document.createElement('button');
+        comprarBtn.classList.add('comprarBtn');
+        comprarBtn.textContent = 'Comprar';
+
+        // Agregamos los elementos al contenedor
+        itemDiv.appendChild(nombreObjetoHeading);
+        itemDiv.appendChild(descripcionP);
+        itemDiv.appendChild(atributoPrecioP);
+        itemDiv.appendChild(atributoDanioP);
+        itemDiv.appendChild(comprarBtn);
+
         listaObjetosContainer.appendChild(itemDiv);
     });
 }
+function comprarObjeto(objeto) {
+    // Obtener los datos del objeto
+    var id = objeto.id;
+    var rareza = objeto.rareza;
+    var nombre = objeto.nombre;
+    var precio = objeto.precio;
+    var damage = objeto.damage;
+    var url = objeto.url;
+    const mail = localStorage.getItem('mail'); 
+    var body = {
+        "id": id,
+        "rareza": rareza,
+        "nombre": nombre,
+        "precio": precio,
+        "damage": damage,
+        "url": url
+    };
 
+    // Enviar la solicitud POST al servidor para comprar el objeto
+    $.post({
+        url: `/dsaApp/tienda/comprarObjeto/${encodeURIComponent(mail)}`,
+        data: JSON.stringify(body),
+        contentType: 'application/json; charset=utf8'
+    })
+        .done(function (data, status) {
+            alert("¡Añadido a tu inventario!");
+            location.href = "/principal.html";
+        })
+        .fail(function (xhr, err) {
+            console.log("ERROR", err);
+            alert("No se ha podido comprar");
+        });
+}
